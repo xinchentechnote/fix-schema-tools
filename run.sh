0c59559f216f8b9ed0919b8b3415b9586550f90c
xinchentechnote <283235702@qq.com>
2026-01-05 20:43:10 +0800

    feat:fix script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 mvn clean spotless:apply package
 mvn exec:java \
   -Dexec.mainClass=com.xinchentechnote.fix.cli.FixSchemaCli \
-  -Dexec.args="generate --xml src/test/resources/FIX44.xml --lang java --out ../quickfixj-lab/fix-codec/src/main/java/ --package com.xinchentechnote.fix.codec"
+  -Dexec.args="generate --xml src/test/resources/FIX44.xml --lang java --out ../quickfixj-lab/fix-codec/src/main/java/ --messages Logon,Advertisement --package com.xinchentechnote.fix.codec"
