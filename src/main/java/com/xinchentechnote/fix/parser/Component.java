adc9f153a86464a43b052024beee54ed651d720e
xinchen <xinchentechnote@163.com>
2026-01-25 22:45:37 +0800

    fix: delete unuse code

--- src/main/java/com/xinchentechnote/fix/parser/Component.java
+++
@@ -1,9 +0,0 @@
-package com.xinchentechnote.fix.parser;
-
-import jakarta.xml.bind.annotation.XmlAttribute;
-import lombok.Data;
-
-@Data
-public class Component extends BaseMessage {
-  @XmlAttribute private String name;
-}
--- src/main/java/com/xinchentechnote/fix/parser/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlAccessorType(XmlAccessType.FIELD)
-package com.xinchentechnote.fix.parser;
-
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
